c74ef9bf8bc213f0233cc73a7f451a02aa171f71
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-07-18 01:40:24 +0600

    get specific user

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const Stripe = require('stripe');
+const Stripe = require("stripe");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
@@ -24,7 +22,7 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-     return res.status(401).send({ message: "Unauthorized" });
+    return res.status(401).send({ message: "Unauthorized" });
   }
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
@@ -33,8 +31,8 @@ const verifyJWT = (req, res, next) => {
     }
     req.user = decoded;
     next();
-  })
-}
+  });
+};
 
 async function run() {
   try {
@@ -43,8 +41,8 @@ async function run() {
     const ArticleCollection = db.collection("article");
     const userCollection = db.collection("users");
 
-// user section 
-     app.get("/users", async (req, res) => {
+    // users section
+    app.get("/users", async (req, res) => {
       try {
         const result = await userCollection.find().toArray();
         res.send(result);
@@ -52,9 +50,22 @@ async function run() {
         console.error("Error fetching publishers:", err);
         res.status(500).send({ error: "Failed to fetch publishers" });
       }
-     });
-    
-    
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email });
+
+      if (user) {
+        return res.send({
+          role: user.role,
+          userType: user.userType,
+        });
+      } else {
+        return res.status(404).send({ message: "User not found" });
+      }
+    });
+
     app.post("/users", async (req, res) => {
       const email = req.body.email;
       const userExists = await userCollection.findOne({ email });
@@ -78,15 +89,12 @@ async function run() {
       res.send(result);
     });
 
-
     // article section
     app.get("/article", async (req, res) => {
       try {
-        const result = await ArticleCollection.find(
-          {
-            status: "pending"  //after create admin role -- status "accepted"
-          }
-        ).toArray();
+        const result = await ArticleCollection.find({
+          status: "pending", //after create admin role -- status "accepted"
+        }).toArray();
         res.send(result);
       } catch (err) {
         console.error("Error fetching publishers:", err);
@@ -94,29 +102,25 @@ async function run() {
       }
     });
 
-    app.get("/article/my-article",verifyJWT, async (req, res) => {
+    app.get("/article/my-article", verifyJWT, async (req, res) => {
       const email = req.query.email;
       const tokenEmail = req.user.email;
       if (email !== tokenEmail) {
-         return res.status(403).send({ message: "Forbidden access" });
+        return res.status(403).send({ message: "Forbidden access" });
       }
-      const result = await ArticleCollection.find(
-        {
-          authorEmail: email
-        }
-      ).toArray();
-      res.send(result)
-    })
+      const result = await ArticleCollection.find({
+        authorEmail: email,
+      }).toArray();
+      res.send(result);
+    });
 
-  
-    app.post("/article",verifyJWT, async (req, res) => {
+    app.post("/article", verifyJWT, async (req, res) => {
       const articleData = req.body;
       const result = await ArticleCollection.insertOne(articleData);
       res.send(result);
     });
 
     // jwt section
-
     app.post("/jwt", (req, res) => {
       const { email } = req.body;
       if (!email) {
@@ -124,26 +128,20 @@ async function run() {
       }
       const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
         expiresIn: "7d",
-      })
-      res.send({token})
-    })
-    
-    // payment section 
-    app.post("/payment/create-payment-intent",async (req, res) => {
+      });
+      res.send({ token });
+    });
+
+    // payment section
+    app.post("/payment/create-payment-intent", async (req, res) => {
       const { price } = res.body;
-      const paymentIntent = await stripe.paymentIntents.create(
-        {
-          amount: price * 100,
-          currency: "usd",
-          payment_method_types: ["card"]
-        }
-      )
-      res.send({clientSecret: paymentIntent.client_secret})
-    })
-
-
-
-   
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price * 100,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
